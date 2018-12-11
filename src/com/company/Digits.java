f3ec972f5816f6cc9ab174e6e60ad93ebeef55c6
BT_1N3_23 <ekorsunsky0407@bths.edu>
2018-12-11 11:37:50 -0500

    Initial commit

---
+++ src/com/company/Digits.java
@@ -0,0 +1,46 @@
+package com.company;
+import java.util.ArrayList;
+
+public class Digits {
+
+        private ArrayList<Integer> digitList;
+
+        public Digits(int num)
+        {
+            digitList = new ArrayList<Integer>();
+
+            if(num == 0)
+            {
+                digitList.add(0);
+            }
+
+            while(num > 0)
+            {
+                digitList.add(0, num % 10);
+                num /= 10;
+            }
+        }
+
+        public boolean isStrictlyIncreasing()
+        {
+            for(int i = 0; i < digitList.size()-1; i++)
+            {
+                if(digitList.get(i) >= digitList.get(i+1))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int[] getDigitList()
+        {
+            int[] arr = new int[digitList.size()];
+            for(int i = 0; i < digitList.size()-1; i++)
+            {
+                arr[i] = digitList.get(i);
+            }
+            return arr;
+        }
+    }
+
